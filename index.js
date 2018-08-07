7986949f7f16bcc66961590b90a7b2d4961e948b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-07 00:56:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function distanceFromHqInBlocks (number) {
     return Math.abs(number - 42)
 }
 
-function distanceFromHqInFeet (someValue) {
-  distanceFromHqInBlocks(someValue);
-  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
-  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+function distanceFromHqInFeet (number) {
+  distanceFromHqInBlocks(number);
+  return number * 264
 }
