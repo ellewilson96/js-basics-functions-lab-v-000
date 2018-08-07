d984290d895b9be4f4cd6d16018ca0275c9b5b6b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-08-07 01:19:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ function distanceFromHqInFeet (blockNumber) {
   return distanceFromHqInBlocks(blockNumber) * 264;
 }
 
-function distanceTravelledInFeet (one, two) {
-  return Math.abs(two - one) * 264;
+function distanceTravelledInFeet (start, destination) {
+  return Math.abs(destination - start) * 264;
 }
 
-function calculatesFarePrice(start, destination) {
-  return distanceFromHqInFeet(start) - distanceFromHqInFeet(destination);
+function calculatesFarePrice (start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return .02 * (distance - 400);
+  } else if (distance > 2000 && distance < 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
 }
